e50911330fc9c8d0e0b79775641941f372a49889
saileshd14 <saileshradar@gmail.com>
2022-11-22 15:38:05 +0530

    P2 OVERRR!!!

--- P2.c
+++ P2.c
@@ -9,7 +9,7 @@
 //
 long long int A [20001];
 long long int B [60001];
-long long int C [30001];
+long long int C [30001]={0};
 long long int size_r = 20;
 long long int size_c = 50;
  
@@ -28,24 +28,26 @@ typedef struct{
  
 void *matrixMul(void *args) {
     pair *x = (pair *) args;
-    //printf("thread %d : st-%d, ed-%d\n", (x->st)/rpt, x->st, x->ed);
+    printf("thread %d : st-%d, ed-%d\n", (x->st)/rpt, x->st, x->ed);
     
     //TO CHECK IF ROWS TO BE READ HAVE BEEN WRITTEN
     //If the last element of a row is -1, then you can't read the row, if it's positive, we can be certain that entire row is read
-    int b=0;
-    while(b==0) {
-        b=1;
-        for(int i = x->st; i < x->ed; i++) {
-            if(A[nAcol*i+nAcol-1]==-1)
-                b=0;
-        }
-        printf("in while loop\n");
-    }
+    // int b=0;
+    // while(b==0) {
+    //     b=1;
+    //     for(int i = x->st; i < x->ed; i++) {
+    //         if(A[nAcol*i+nAcol-1]==-1)
+    //             b=0;
+    //     }
+    //     printf("in while loop\n");
+    // }
     //CALCULATIONS
     for (int i = x->st; i < x->ed; i++) {
         for (int j = 0; j < nBcol; j++) {
             for (int k = 0; k < nAcol; k++) {
                 C[nBcol*i+j] += A[nAcol*i+k] * B[nBcol*k+j];
+                // printf("A in i : %d,  j : %d , k : %d is : %lld\n", i, j, k,  A[nAcol*i+k]);
+                // printf("B in i : %d,  j : %d , k : %d is : %lld\n", i, j, k,  B[nAcol*i+k]);
             }
         }
     }
@@ -73,8 +75,7 @@ int main(int argc, char *argv[]) {
     gettimeofday(&start, NULL);
  
     nArow = 20;
-    nAcol = 50;
-    nBrow = 50;
+    nAcol = nBrow = 50;
     nBcol = 20;
  
     // for(int i=0;i<nAcol*nArow;i++)
@@ -82,25 +83,9 @@ int main(int argc, char *argv[]) {
  
     // for(int i=0;i<nBcol*nBrow;i++)
     //     B[i]=i+1;
-    
-    FILE *inpfile;
-    inpfile = fopen("in1.txt", "r");
     long long int num;
     char ch;
     long long int j=0;
-    for(int x=0;x<nArow;x++) {
-        j=0;
-        while (fscanf(inpfile, "%lld%c", &num, &ch)==2){
-            //do something
-			A[nAcol*(x)+j] = num;
-			printf("%lld ",num);
-            if(ch == '\n')
-                break;
-	    	j++;
-        }
-        printf("\n");
-    }
- 
     FILE *inpfile1;
     inpfile1 = fopen("in2.txt", "r");
     j=0;
@@ -108,15 +93,30 @@ int main(int argc, char *argv[]) {
         j=0;
         while (fscanf(inpfile1, "%lld%c", &num, &ch)==2){
             //do something
-			B[nBcol*(x)+j] = num;
-			printf("%lld ",num);
+			B[nBcol*x+j] = num;
+            if(ch == '\n' | ch==EOF)
+                break;
+	    	j++;
+        }
+    }
+    fclose(inpfile1);
+
+    FILE *inpfile;
+    inpfile = fopen("in1.txt", "r");
+    
+    for(int x=0;x<nArow;x++) {
+        j=0;
+        while (fscanf(inpfile, "%lld%c", &num, &ch)==2){
+            //do something
+			A[nAcol*(x)+j] = num;
             if(ch == '\n')
                 break;
 	    	j++;
         }
-        printf("\n");
     }
-
+    fclose(inpfile);
+    
+    printf("AAAAAAAAAAAAAAAAAA\n");
     for (int i = 0; i < nArow; i++) {
         for (int j = 0; j < nAcol; j++) {
             printf("%lld ", A[i*nAcol+j]);
@@ -124,7 +124,8 @@ int main(int argc, char *argv[]) {
         printf("\n");
     }
     printf("\n");
- 
+
+    printf("BBBBBBBBBBBBBBBBBB\n");
     for (int i = 0; i < nBrow; i++) {
         for (int j = 0; j < nBcol; j++) {
             printf("%lld ", B[i*nBcol+j]);
@@ -140,7 +141,7 @@ int main(int argc, char *argv[]) {
  
     //one thread for each column
  
-    int num_threads = 15;
+    int num_threads = 3;
 
     if(num_threads>nArow){
         num_threads=nArow;
@@ -149,36 +150,34 @@ int main(int argc, char *argv[]) {
     //num_threads = num_threads > nArow ? nArow : num_threads;
  
     rpt = (nArow / num_threads) + 1; // rows per thread
+
+    long long int ceil_rpt = nArow / num_threads + 1; //4
+    long long int flr_rpt = nArow / num_threads; //3
+
+    long long int rem = nArow % num_threads; // 1
+
+    //num threads = 2, nArows = 5
+    // 10 threads x 4 rows and 20 threads x 3 rows
  
-    for(int l = 0; l<num_threads-1;l++) {
-        // for (int i = l * rpt; i < (l+1) * rpt; i++) {
-        //     for (int j = 0; j < nBcol; j++) {
-        //         for (int k = 0; k < nAcol; k++) {
-        //             C[nBcol*i+j] += A[nAcol*i+k] * B[nBcol*k+j];
-        //         }
-        //     }
-        // }
+    for(int l = 0 ; l<rem ; l++) { //
         //INITIALIZE ARRAY OF ARG BEFORE HAND?
         pair * arg = (pair *) malloc(sizeof(pair));
-        arg->st = l * rpt;
-        arg->ed = (l+1) * rpt;
-        //printf("in main : thread %d : st-%d, ed-%d\n", l, arg->st, arg->ed);
+        arg->st = l * ceil_rpt;
+        arg->ed = (l+1) * ceil_rpt;
+        printf("in main 1 : thread %d : st-%d, ed-%d\n", l, arg->st, arg->ed);
         pthread_create(&threads[l], NULL, matrixMul, (void *) arg);
     }
- 
-    // for (int i = (num_threads-1) * rpt; i < nArow; i++) {
-    //     for (int j = 0; j < nBcol; j++) {
-    //         for (int k = 0; k < nAcol; k++) {
-    //             C[nBcol*i+j] += A[nAcol*i+k] * B[nBcol*k+j];
-    //         }
-    //     }
-    // }
- 
-    //pair arg = { (num_threads-1) * rpt, nArow };
-    pair * arg = (pair *) malloc(sizeof(pair));
-    arg->st = (num_threads-1) * rpt;
-    arg->ed = nArow;
-    pthread_create(&threads[num_threads-1], NULL, matrixMul, (void *) arg);
+
+    printf("ceil %lld\n", rem*ceil_rpt);
+    printf("l lim %lld\n", num_threads-rem);
+    for(int l = rem; l<num_threads; l++) {
+        pair * arg = (pair *) malloc(sizeof(pair));
+        arg->st = l * flr_rpt + rem * (ceil_rpt-flr_rpt);
+        arg->ed = (l+1) * flr_rpt + rem * (ceil_rpt-flr_rpt);
+        printf("in main 2 : thread %d : st-%d, ed-%d\n", l, arg->st, arg->ed);
+        pthread_create(&threads[l], NULL, matrixMul, (void *) arg);
+    }
+
  
     for (int i = 0; i < num_threads; i++) {
         pthread_join(threads[i], NULL);
